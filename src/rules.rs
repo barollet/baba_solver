8eb814696487029547ddc24215a331ec3d357203
theo barollet <theo.barollet@grenoble-inp.org>
2019-09-25 11:56:24 +0200

    Structural update

--- src/rules.rs
+++ src/rules.rs
@@ -1,19 +1,56 @@
 //! A rule manager, the rules are manually updated by the level interface
 
+use std::ops::{Index, IndexMut};
+use std::convert::TryFrom;
+
+use either::*;
+
 use crate::square::*;
-use std::collections::HashMap;
-
-
-// Conversion between unitary squares and the according subtype
 
 pub struct RuleManager {
-    rules: HashMap<Square, Square>,
+    rules: [Vec<Text>; Text::VARIANT_COUNT],
 }
 
 impl Default for RuleManager {
     fn default() -> Self {
-        Self {
-            rules: HashMap::new(),
+        let mut manager = Self {
+            rules: array_init::array_init(|_| vec!()),
+        };
+        manager.add_rule(Text::TEXT, Text::PUSH);
+
+        manager
+    }
+}
+
+impl Index<Text> for RuleManager {
+    type Output = Vec<Text>;
+
+    fn index(&self, text: Text) -> &Self::Output {
+        &self.rules[text as usize]
+    }
+}
+
+impl IndexMut<Text> for RuleManager {
+    fn index_mut<'a>(&'a mut self, text: Text) -> &'a mut Self::Output {
+        &mut self.rules[text as usize]
+    }
+}
+
+impl RuleManager {
+    pub fn add_rule(&mut self, entity: Text, property: Text) {
+        if !self[entity].contains(&property) {
+            self[entity].push(property);
         }
+    }
+
+    pub fn has_property(&self, square: Square, property: Text) {
+        self[Text::from(Either::try_from(square))].contains(&property);
+    }
+
+    /// Returns if the given square type is crossable by something
+    // A square is crossable if it is not STOP and can be PUSHed
+    pub fn is_crossable(&self, square: Square) -> bool {
+        // TODO
+        true
     }
 }
--- src/square.rs
+++ src/square.rs
@@ -3,6 +3,7 @@
 use either::*;
 use enum_primitive::FromPrimitive;
 use std::convert::TryFrom;
+use variant_count::VariantCount;
 
 const FIRST_ENTITY_INDEX: u32 = 10;
 bitflags! {
@@ -27,33 +28,54 @@ bitflags! {
 
 enum_from_primitive! {
     #[repr(u8)]
+    #[derive(PartialEq, Eq, Hash, VariantCount, Clone, Copy, Debug)]
+    // We keep the same order as in Entity so we can quickly convert between the two types
     pub enum Text {
         BABA = 0,
+        FLAG,
+        WALL,
+        ROCK,
         IS,
         YOU,
-        FLAG,
         WIN,
-        WALL,
         STOP,
-        ROCK,
         PUSH,
+
+        // Special value to handle rules with text
+        // TODO
+        TEXT,
+        EMPTY,
     }
 }
 
 enum_from_primitive! {
     #[repr(u8)]
+    #[derive(Clone, Copy, Debug)]
     pub enum Entity {
         BABA = 0,
-        ROCK,
         FLAG,
         WALL,
+        ROCK,
     }
 }
 
-pub type SquareConversionError = ();
+pub type ConversionError = ();
+
+impl From<Text> for Entity {
+    fn from(text: Text) -> Self {
+        Self::from_u8(text as u8).expect("Cannot convert text into entity")
+    }
+}
+
+impl TryFrom<Entity> for Text {
+    type Error = ConversionError;
+    fn try_from(entity: Entity) -> Result<Self, Self::Error> {
+        Self::from_u8(entity as u8).ok_or(())
+    }
+}
 
 impl TryFrom<Square> for Text {
-    type Error = SquareConversionError;
+    type Error = ConversionError;
     fn try_from(square: Square) -> Result<Self, Self::Error> {
         let index = square.bits().trailing_zeros();
         Text::from_u32(index).ok_or(())
@@ -61,7 +83,7 @@ impl TryFrom<Square> for Text {
 }
 
 impl TryFrom<Square> for Entity {
-    type Error = SquareConversionError;
+    type Error = ConversionError;
     fn try_from(square: Square) -> Result<Self, Self::Error> {
         let index = square.bits().trailing_zeros();
         Entity::from_u32(index - FIRST_ENTITY_INDEX).ok_or(())
@@ -69,12 +91,21 @@ impl TryFrom<Square> for Entity {
 }
 
 impl TryFrom<Square> for Either<Text, Entity> {
-    type Error = SquareConversionError;
+    type Error = ConversionError;
     fn try_from(square: Square) -> Result<Self, Self::Error> {
         match Text::try_from(square) {
             Ok(text) => Ok(Either::Left(text)),
-            Err(()) => Entity::try_from(square).map(|entity| Either::Right(entity)),
+            Err(()) => Entity::try_from(square).map(Either::Right),
         }
+    }
+}
+
+impl From<Result<Either<Text, Entity>, ()>> for Text {
+    fn from(res: Result<Either<Text, Entity>, ()>) -> Self {
+        res.unwrap_or(either::Either::Left(Text::EMPTY)).either(
+            |text| text,
+            |entity| Text::try_from(entity).expect("Cannot convert entity to text"),
+        )
     }
 }
 
@@ -83,9 +114,7 @@ impl IntoIterator for Square {
     type IntoIter = SquareIterator;
 
     fn into_iter(self) -> Self::IntoIter {
-        SquareIterator {
-            value: self.bits(),
-        }
+        SquareIterator { value: self.bits() }
     }
 }
 
